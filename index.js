7e177ba4255c8af38e8fb60294ad49479a9ecd9c
Luis Perez <Luis998.ap@gmail.com>
2026-02-23 14:35:31 -0600

    created HTTP server for DiceRoller using node CORE module and implemented logic

---
+++ index.js
@@ -0,0 +1,33 @@
+import http from 'http';
+const port = process.env.PORT || 8080
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/roll' && req.method === 'GET'){
+        let numStorage = []
+        for(let i = 0; i < 3; i++){
+            numStorage.push(Math.floor(Math.random() * 6) + 1);
+        }
+        let total = numStorage.reduce((sum, num) => sum + num, 0)
+        let win = total === 11;
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        });
+        res.end(JSON.stringify({numStorage, total, win}));
+    }
+    else{
+        res.writeHead(404,{'Content-Type': 'text/plain'});
+        res.end('Not Found')
+    }
+})
+
+server.listen(port, function(error){
+    if (error){
+        console.log('Something went wrong', error)
+
+    }
+    else{
+        console.log('Server is listening on port ' + port)
+    }
+})
