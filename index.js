bddd9d7bfb2dd392956880d490a64a9630531a4f
Luis Perez <Luis998.ap@gmail.com>
2026-02-28 23:05:30 -0600

    created different nodes for result and reset

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
 import http from 'http';
 const port = process.env.PORT || 5501
-const storage = []
+let storage = []
 const server = http.createServer((req, res) => {
-    if (req.url === '/roll' && req.method === 'GET'){
+    const headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET'
+    }
 
-        let number = ``;
+    if (req.url === '/roll' && req.method === 'GET'){
+        let number = '';
         let msg = '';
-        let result = '';
         if (storage.length !== 3){
             number = Math.floor(Math.random() * 6) + 1;
+            console.log(number)
             storage.push(number);
         }
-        else if (storage.length === 3){
+        if (storage.length === 3){
             msg = "You already rolled 3 times";
-            let total = storage.reduce((sum, num) => sum + num, 0);
-            if (total === 11){
-                result = "true";
-            }
-            else{
-                result = "false";
-            }
         }
 
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET');
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        });
-        res.end(JSON.stringify({number,msg, result}));
+        res.writeHead(200, headers);
+        res.end(JSON.stringify({number, msg}));
+    }
+    else if (req.url === '/result' && req.method === 'GET'){
+        let result = '';
+        let total = storage.reduce((sum, num) => sum + num, 0);
+        if (total === 11){
+            result = "true";
+        }
+        else{
+            result = "false";
+        }
+        res.writeHead(200, headers);
+        res.end(JSON.stringify({result}));
     }
     else if (req.url === '/reset' && req.method === 'GET'){
-        storage = []
+        storage = [];
+        res.writeHead(200, headers);
+        res.end();
     }
     else{
         res.writeHead(404,{'Content-Type': 'text/plain'});
         res.end('Not Found')
     }
 })
-
 server.listen(port, function(error){
     if (error){
         console.log('Something went wrong', error)
